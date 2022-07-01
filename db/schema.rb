0f66bbf1a1e183a500a51826d828d04a8d9c7fb1
Megan Van Der Weide <megan.vanderweide@gmail.com>
2022-07-01 15:43:41 +1000

    allergies model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_30_041446) do
+ActiveRecord::Schema.define(version: 2022_07_01_052614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,12 @@ ActiveRecord::Schema.define(version: 2022_06_30_041446) do
     t.string "street_name"
     t.string "suburb"
     t.integer "postcode"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "allergies", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
