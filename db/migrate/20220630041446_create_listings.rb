7853a5a1a3213f77ce2417f47c7022f46fc8392f
Megan Van Der Weide <megan.vanderweide@gmail.com>
2022-07-01 16:02:59 +1000

    foreign key fix from listings to allergy model

--- db/migrate/20220630041446_create_listings.rb
+++ db/migrate/20220630041446_create_listings.rb
@@ -3,10 +3,10 @@ class CreateListings < ActiveRecord::Migration[6.1]
     create_table :listings do |t|
       t.string :title
       t.string :cuisine
-      t.string :allergy
       t.integer :price
       t.string :available_date
       t.boolean :sold, default: false
+      t.references :allergy, null:false, foreign_key: true
 
       t.timestamps
     end
